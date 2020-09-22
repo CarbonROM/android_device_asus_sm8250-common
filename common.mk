8c746687fdfbdea3242ab495ce861b3390f2a589
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    sm8250-common: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- common.mk
+++ common.mk
@@ -181,7 +181,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
